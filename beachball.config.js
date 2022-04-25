1f56638e9b6ba8c9f1636b68a5cb1d72f0c3dd0b
Erick Tamayo <ericktamayo@gmail.com>
2022-04-25 17:51:07 -0600

    updated beachball config

--- beachball.config.js
+++ beachball.config.js
@@ -2,6 +2,13 @@
  * @type {import('beachball').BeachballConfig}
  */
 module.exports = {
+  bumpDeps: true,
+  groups: [
+    {
+      name: "all",
+      include: ["packages/*"],
+    },
+  ],
   access: "public",
   branch: "main",
 };
