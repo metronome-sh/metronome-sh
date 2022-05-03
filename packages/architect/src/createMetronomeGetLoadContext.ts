4e57959eccd4cb84591712b8b86b611051fcc532
Erick Tamayo <ericktamayo@gmail.com>
2022-05-03 17:24:38 -0600

    removed invariant

--- packages/architect/src/createMetronomeGetLoadContext.ts
+++ packages/architect/src/createMetronomeGetLoadContext.ts
@@ -1,6 +1,4 @@
 import type { ServerBuild } from "@remix-run/server-runtime";
-import invariant from "ts-invariant";
-import path from "path";
 import type { APIGatewayProxyEventV2 } from "aws-lambda";
 import { NodeSpan, SpanName, NodeSpanExporter } from "@metronome-sh/node";
 import {
@@ -9,24 +7,16 @@ import {
 } from "@metronome-sh/runtime";
 
 export const createMetronomeGetLoadContext = (build: ServerBuild) => {
-  const arcConfig = process.env.__ARC_CONFIG__;
-
-  invariant(arcConfig, "Couldn't get Architect configuration");
-
   const exporter = new NodeSpanExporter({
     apiKey: process.env.METRONOME_API_KEY,
     metronomeUrl: process.env.METRONOME_URL,
     metronomeDebug: process.env.METRONOME_DEBUG,
   });
 
-  const { projectSrc } = JSON.parse(arcConfig);
-
-  const { version } = require(path.resolve(projectSrc, "package.json"));
-
   const { version: hash } = build.assets;
   const { version: metronomeVersion } = require("../package.json");
 
-  const projectMeta = { version, hash, metronomeVersion };
+  const projectMeta = { version: "", hash, metronomeVersion };
 
   return (request: APIGatewayProxyEventV2): ContextWithMetronome => {
     if (process.env.NODE_ENV !== "production") return {};
