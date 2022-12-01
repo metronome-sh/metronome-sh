0433f658fa4b901bf8dade53d75a83aee80f2f5c
Erick Tamayo <ericktamayo@gmail.com>
2022-12-01 11:41:47 -0700

    added nonce support

--- packages/react/src/components/MetronomeLinks.tsx
+++ packages/react/src/components/MetronomeLinks.tsx
@@ -3,11 +3,15 @@ import type { VoidFunctionComponent } from "react";
 const metronomeVersion = "__METRONOME_VERSION__";
 const webVitalsPolyfill = "__WEB_VITALS_POLYFILL__";
 
+export type MetronomeLinksProps = {
+  nonce?: string;
+}
+
 // Scripts adds a 2kb for initial loading.
-export const MetronomeLinks: VoidFunctionComponent =
+export const MetronomeLinks: VoidFunctionComponent<MetronomeLinksProps> =
   process.env.NODE_ENV === "development"
     ? () => null
-    : () => {
+    : ({ nonce }) => {
         return (
           <>
             <script
@@ -18,6 +22,7 @@ export const MetronomeLinks: VoidFunctionComponent =
             <script
               src={`/__metronome/metronome-${metronomeVersion}.js`}
               defer
+              nonce={nonce}
             />
           </>
         );
